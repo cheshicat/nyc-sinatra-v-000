0c1e2f08c2588feebd5a1b6311eb491793a9a3db
Allison Howard <alliemhow@gmail.com>
2019-05-24 09:18:25 +0000

    Automatically backed up by Learn

--- db/migrate/02_create_figures.rb
+++ db/migrate/02_create_figures.rb
@@ -3,8 +3,6 @@ class CreateFigures < ActiveRecord::Migration
     create_table :figures do |t|
       t.integer :id
       t.string :name
-      t.integer :figure_id
-      t.integer :year_completed
     end
   end
 end
