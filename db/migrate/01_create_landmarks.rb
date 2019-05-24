1e90845dbe32321489e15b9c76aefcacf784a97e
Allison Howard <alliemhow@gmail.com>
2019-05-24 09:23:22 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,7 +1,6 @@
 class CreateLandmarks < ActiveRecord::Migration
   def change
     create_table :landmarks do |t|
-      t.integer :id
       t.string :name
       t.integer :figure_id
       t.integer :year_completed
